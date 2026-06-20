c5b393bb2efd4925b9e08b3c038d8800fce3416b
rhorn86 <rhorn86@gmail.com>
2026-06-19 20:13:25 -1000

    added image and link splitting functions to splitdelimiter.py

--- src/splitdelimiter.py
+++ src/splitdelimiter.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, TextType
+from extract import extract_markdown_images, extract_markdown_links
 
 def split_nodes_delimiter(
         old_nodes: list[TextNode],
@@ -29,7 +30,54 @@ def split_nodes_delimiter(
 
     return new_nodes
 
+def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
+    new_nodes = []
+    for old_node in old_nodes:
+        remaining_text = old_node.text
+        if old_node.text_type is not TextType.TEXT:
+            new_nodes.append(old_node)
+        else:
+            matches = extract_markdown_images(old_node.text)
+            if not matches and old_node.text:
+                new_nodes.append(old_node)
+            else:
+                for alt_text, url in matches:
+                    delimiter = f"![{alt_text}]({url})"
+                    split_text = remaining_text.split(delimiter, 1)
+                    left = TextNode(split_text[0], TextType.TEXT)
+                    center = TextNode(alt_text, TextType.IMAGE, url)
+                    if left.text:
+                        new_nodes.append(left)
+                    new_nodes.append(center)
+                    remaining_text = split_text[1]
+                right = TextNode(remaining_text, TextType.TEXT)
+                if right.text:
+                    new_nodes.append(right)
+    return new_nodes
 
 
-
+def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
+    new_nodes = []
+    for old_node in old_nodes:
+        remaining_text = old_node.text
+        if old_node.text_type is not TextType.TEXT:
+            new_nodes.append(old_node)
+        else:
+            matches = extract_markdown_links(old_node.text)
+            if not matches and old_node.text:
+                new_nodes.append(old_node)
+            else:
+                for anchor_text, url in matches:
+                    delimiter = f"[{anchor_text}]({url})"
+                    split_text = remaining_text.split(delimiter, 1)
+                    left = TextNode(split_text[0], TextType.TEXT)
+                    center = TextNode(anchor_text, TextType.LINK, url)
+                    if left.text:
+                        new_nodes.append(left)
+                    new_nodes.append(center)
+                    remaining_text = split_text[1]
+                right = TextNode(remaining_text, TextType.TEXT)
+                if right.text:
+                    new_nodes.append(right)
+    return new_nodes
 
